1ac48ba96302491dcf0bcb5857c3b5007a9d82b9
mans4321 <t6t6t.sa@gmail.com>
2020-09-20 08:38:01 -0400

    tickTok clone

--- frontEnd/src/Firbase/Firbase.js
+++ frontEnd/src/Firbase/Firbase.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDxdHVOh4-B8wHvwN3XinROf8AmIrE_-c8",
-    authDomain: "tik-tok-clone-f008b.firebaseapp.com",
-    databaseURL: "https://tik-tok-clone-f008b.firebaseio.com",
-    projectId: "tik-tok-clone-f008b",
-    storageBucket: "tik-tok-clone-f008b.appspot.com",
-    messagingSenderId: "257811173570",
-    appId: "1:257811173570:web:db062a8afff86bfa9c6b5b",
-    measurementId: "G-ZBD5NTGP5Y"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
